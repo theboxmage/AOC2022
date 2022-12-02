b3326817be56ff07c04d5dec736035dd8a7af74f
theboxmage <joshua.h.graham96@gmail.com>
2022-12-02 01:33:40 -0500

    Fixes spacing

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -22,12 +22,12 @@ class Day2(private val file: String) {
     }
 
     private fun runGamePart2(opponent: Char, player: Char): Int {
-        return  (if(player == 'Y') {
+        return (if(player == 'Y') {
             letterScore[opponent]!!
         } else if(player == 'X') {
             letterScore[winMap[opponent]!!.second]!!
         } else {
             letterScore[winMap[opponent]!!.first]!!
-        })+ resultScore[player]!!
+        }) + resultScore[player]!!
     }
 }
