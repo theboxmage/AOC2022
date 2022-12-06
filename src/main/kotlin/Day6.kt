e11ba838f5c5fa2a29ef8033f52ab8872f155a3c
theboxmage <joshua.h.graham96@gmail.com>
2022-12-06 00:12:56 -0500

    Better naming convention?

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -2,14 +2,14 @@ class Day6(file: String) {
     private var item = FileUtils.readFileAsLines(file)[0]
 
     fun part1() {
-        println("Part 1: ${checkDistinct(4)}")
+        println("Part 1: ${findDistinctIndex(4)}")
     }
 
     fun part2() {
-        println("Part 2: ${checkDistinct(14)}")
+        println("Part 2: ${findDistinctIndex(14)}")
     }
 
-    private fun checkDistinct(size: Int): Int {
+    private fun findDistinctIndex(size: Int): Int {
         for(i in 0 until item.length-size) {
             if(item.substring(i, i+size).toSet().count() == size) {
                 return i+size
