d4b718f2afe593e061505fff904150d6ff7e76ba
theboxmage <joshua.h.graham96@gmail.com>
2022-12-08 11:41:15 -0500

    Brute forced part 2. There's a more elegant solution to do a lot of things, but I'm not looking for it today

---
+++ src/main/kotlin/Day8.kt
@@ -0,0 +1,68 @@
+class Day8 : Day {
+    private val input =
+        FileUtils.readFileAsLines("day8.txt").map { it.split("").map { i -> i.toIntOrNull() }.filterNotNull() }
+    private val p = (input.size - 1) * 4
+
+    override fun part1() {
+        var count = p
+        for(i in 1 until input.size-1) {
+            for(j in 1 until input[0].size-1) {
+                count += countVisibileFromExterior(j, i)
+            }
+        }
+        println("Part 1: $count")
+    }
+
+    override fun part2() {
+        var max = -1
+        for(y in 0 until input.size) {
+            for(x in 0 until input[y].size) {
+                val curr = countVisibleFromNode(y, x)
+                if(max < curr) {
+                    max = curr
+                }
+            }
+        }
+        println("Part 2: $max")
+    }
+
+    fun countVisibileFromExterior(x: Int, y: Int): Int {
+        var count = 0
+        if ((0 until x).all { input[y][x] > input[y][it] }) count = 1
+        else if ((x+1 until input.size).all { input[y][x] > input[y][it] }) count = 1
+        else if ((0 until y).all { input[y][x] > input[it][x] }) count = 1
+        else if ((y+1 until input.size).all { input[y][x] > input[it][x] }) count = 1
+        return count
+    }
+
+    private fun countVisibleFromNode(x: Int, y: Int): Int {
+        val count = arrayOf(0, 0, 0, 0)
+        for(i in x-1 downTo  0) {
+            count[0]++
+            if(input[y][x] <= input[y][i]) {
+                break
+            }
+        }
+        for(i in x+1 until  input.size) {
+            count[1]++
+            if(input[y][x] <= input[y][i]) {
+                break
+            }
+        }
+        for(i in y-1 downTo  0) {
+            count[2]++
+            if(input[y][x] <= input[i][x]) {
+                break
+            }
+        }
+        for(i in y+1 until  input[x].size) {
+            count[3]++
+            if(input[y][x] <= input[i][x]) {
+                break
+            }
+        }
+        val value = count[0]*count[1]*count[2]*count[3]
+        return value
+    }
+}
+
